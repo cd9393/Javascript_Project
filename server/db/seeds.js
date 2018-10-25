28de76c16a7580326839e04727b4d0d142f7f975
Raymond Yau <raymondccyau@gmail.com>
2018-10-25 16:46:06 +0100

    start files

---
+++ server/db/seeds.js
@@ -0,0 +1,19 @@
+use coin_tracker
+db.dropDatabase();
+
+db.coins.insertMany([
+  {
+    name: "Ethereum",
+    amount: 5,
+  },
+
+  {
+    name: "Bitcoin",
+    amount: 1,
+  },
+
+  {
+    name: "Litecoin",
+    amount: 100,
+  }
+])
