cc9a8943b2d66e458a1f649b8ff1f7d3278f478e
Craig Dunlop <craigdun9393@gmail.com>
2018-10-27 15:25:21 +0100

    created total value view

--- client/src/views/total_value_view.js
+++ client/src/views/total_value_view.js
@@ -7,14 +7,22 @@ const TotalValueView = function(container){
 TotalValueView.prototype.bindEvents = function () {
   PubSub.subscribe('Coin:Portfolio-Loaded', (event) => {
     const portfolio = event.detail;
+
     const portfolioValue = portfolio.map((coin) => {
       return coin.value
     }).reduce((accumulator, currentValue) => accumulator + currentValue)
+
+    this.render(portfolioValue)
+
   })
 };
 
-TotalValueView.prototype.render = function () {
-  
+TotalValueView.prototype.render = function (amount) {
+  const valueDiv = document.createElement('div');
+  const value = document.createElement('h1');
+  value.textContent = `Portfolio Value : $ ${amount}`
+  valueDiv.appendChild(value)
+  this.container.insertBefore(valueDiv,this.container.firstChild)
 };
 
 module.exports = TotalValueView
